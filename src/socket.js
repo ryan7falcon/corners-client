372444456d938e3ac52e7414dac2748c0963bae5
ryan7falcon <ryan7falcon@gmail.com>
2025-01-17 13:34:11 -0500

    socket url

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : `http://${window.location.hostname}:3001`
+const URL = process.env.NODE_ENV === 'production' ? `http://${window.location.hostname}:3001` : `http://${window.location.hostname}:3001`
 
 export const socket = io(URL)
